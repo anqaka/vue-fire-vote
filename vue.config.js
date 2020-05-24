87b96010b7bcd72393069dd1f5509459612a1418
Ania <anna.karon@snow.dog>
2020-05-25 00:51:12 +0200

    update chunk size

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
 const path = require('path')
 module.exports = {
-  configureWebpack: {
-    optimization: {
-      splitChunks: {
-        minSize: 10000,
-        maxSize: 250000
-      }
-    }
-  },
   css: {
     loaderOptions: {
       sass: {
