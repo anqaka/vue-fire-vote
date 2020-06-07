e8b847df9137d08cd2341c4f96b5133b2a6efe3b
Ania <anna.karon@snow.dog>
2020-06-07 15:41:31 +0200

    update title and add explanation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <main id="app" class="container">
     <div class="row center-xs">
-      <h1>
-        Topic propositions for MMPL2020
+      <h1 class="col-xs-12">
+        List of proposed topics for MMPL2020
       </h1>
+      <div class="col-xs-12">
+        <p>Some text that explain what is the page for</p>
+        <p>
+          This page allows as to collect topic that congerence's participants are interested in. <br> It helps speakers to choose their subjects and us t choose thise which fit your need. <br>
+          You have to be logged in to vote for a topic or add your own proposition.
+        </p>
+      </div>
       <topic-list class="col-xs-12" />
       <user-state class="col-xs-12" />
       <add-topic
