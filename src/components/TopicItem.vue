14c67407c78157daea3602f632c9a52f2f460222
Ania <anna.karon@snow.dog>
2020-06-08 00:30:30 +0200

    add tooltip, update vote flow

--- src/components/TopicItem.vue
+++ src/components/TopicItem.vue
@@ -19,24 +19,33 @@
         :class="[
           'topics-list-item__upvote',
           {
-            'topics-list-item__upvote--voted': isVoted
+            'topics-list-item__upvote--voted': isVoted,
+            'topics-list-item__upvote--disabled': !voteEnable
           }
         ]"
-        :disabled="voteEnable"
         @btn-event="isVoted ? downvote(id, votes) : upvote(id, votes)"
+        v-tooltip="tooltipText"
+        :aria-label="tooltipText"
       >
-        {{ actionText }}
+        <span class="button__icon">
+          <LikeIcon />
+        </span>
       </v-button>
     </div>
   </li>
 </template>
 <script>
+import Vue from 'vue'
 import { mapGetters, mapState } from 'vuex'
+import { VTooltip } from 'v-tooltip'
 import VButton from '@/components/Button.vue'
+
+Vue.directive('tooltip', VTooltip)
 
 export default {
   components: {
-    VButton
+    VButton,
+    LikeIcon: () => import('@/assets/icons/like.svg')
   },
   props: {
     description: {
@@ -71,26 +80,40 @@ export default {
     isVoted () {
       return this.userVotes.some(item => item[this.id])
     },
-    actionText () {
-      return this.isVoted ? 'Downvote' : 'Upvote'
-    },
     voteEnable () {
-      return !this.isLoggedIn || (this.author === this.user.id)
+      return !this.isLoggedIn || (this.author !== this.user.id)
+    },
+    tooltipText () {
+      let message = ''
+      if (!this.isLoggedIn) {
+        message = 'You have to be logged in to vote'
+      } else if ((this.author === this.user.id)) {
+        message = 'Naughty naughty!<br> It\'s not fair to vote for your own topic.'
+      } else if (this.isVoted) {
+        message = 'Voted!'
+      }
+      return message
     }
   },
   methods: {
     upvote (id, votes) {
-      this.$store.dispatch('upvote', { id, votes })
+      if (this.voteEnable) {
+        this.$store.dispatch('upvote', { id, votes })
+      }
     },
     downvote (id, votes) {
-      const userVote = this.userVotes.find(item => item[id] === true)
-      const itemKey = userVote['.key']
-      this.$store.dispatch('downvote', { id, itemKey, votes })
+      if (this.voteEnable) {
+        const userVote = this.userVotes.find(item => item[id] === true)
+        const itemKey = userVote['.key']
+        this.$store.dispatch('downvote', { id, itemKey, votes })
+      }
     }
   }
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+@import './src/assets/scss/tooltip';
+
 .topics-list-item {
   display: flex;
   flex-flow: column nowrap;
@@ -118,7 +141,7 @@ export default {
   &__vote {
     display: flex;
     flex-flow: row nowrap;
-    align-items: flex-start;
+    align-items: center;
     justify-content: flex-end;
   }
 
@@ -126,12 +149,33 @@ export default {
     padding: $spacer--m;
   }
 
+  &__vote-info {
+    padding: 0 $spacer--s;
+  }
+
   &__upvote {
+    padding: 0 $spacer--xs;
     margin: $spacer--m;
-    min-width: 100px;
+    min-width: 56px;
+    height: 56px;
+
+    .button__icon {
+      fill: $orange;
+      width: 32px;
+      height: 32px;
+    }
 
     &--voted {
       background-color: $orange;
+      border-color: $orange;
+
+      .button__icon {
+        fill: $black;
+      }
+    }
+
+    &--disabled {
+      @extend .button:disabled;
     }
   }
 }
